5f9ee1e9d1632401a8f03b39b05fb6bea4a3bdbb
Cyril Desbiens <48460444+Koadek@users.noreply.github.com>
2020-04-22 14:03:33 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'sGgznYKoYj66DSoIjOqZuiigh',
+  consumer_secret: 'FZgabcPWTNqvrAG19a0ju3Zhzi9qMRgYInDJoCvB43GNhW6LPR',
+  access_token_key: '489525405-HaTSOUOqqKhgObKmRB2Jwcg6CuQpC75Y6y7u14OI',
+  access_token_secret: '0yEXSNrWcGgs6QsjGDVVCSTXEiaHxhpHcD0sTb6Du9jRP',
+};
